72d9e2dd4f00499c26f92476272324622fe09f81
Santhosh_Kumar_Nayak <santhoshkn011@gmail.com>
2022-07-03 23:22:04 +0530

    Added: Calculate the total permutaion of string

--- src/com/bridgelabz/AlgorithmProblems.java
+++ src/com/bridgelabz/AlgorithmProblems.java
@@ -8,6 +8,34 @@ import java.util.Scanner;
 
 public class AlgorithmProblems {
     public static void main (String[]args) {
-        System.out.println("Welcome to Algorithm Programs");
+        System.out.println("Enter your string: ");
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        int n = str.length(); //length of the string
+        AlgorithmProblems permutation = new AlgorithmProblems(); //Created object
+        permutation.permute(str);
+    }
+    private void permute (String str) {
+        if (str == null || str.length() == 0) {
+            return;
+        }
+        permutations(str.toCharArray(), 0);
+    }
+    //method to permute the string
+    private static void permutations(char[] chars, int currentIndex) {
+        if (currentIndex == chars.length - 1) {
+            System.out.println(String.valueOf(chars));
+        }
+        for (int i = currentIndex; i < chars.length; i++) {
+            swap(chars, currentIndex, i);
+            permutations(chars, currentIndex + 1);
+            swap(chars, currentIndex, i);
+        }
+    }
+    //method to swap
+    private static void swap(char[] chars, int i, int j) {
+        char temp = chars[i];
+        chars[i] = chars[j];
+        chars[j] = temp;
     }
 }
