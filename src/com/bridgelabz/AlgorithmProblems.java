a76953256442dadd7b03dd8d6679409fac8bce31
Santhosh_Kumar_Nayak <santhoshkn011@gmail.com>
2022-07-03 23:18:48 +0530

    Welcome Message

---
+++ src/com/bridgelabz/AlgorithmProblems.java
@@ -0,0 +1,13 @@
+/*
+Write static functions to return all permutations of a String using iterative method and
+Recursion method. Check if the arrays returned by two string functions are equal.
+ */
+package com.bridgelabz;
+
+import java.util.Scanner;
+
+public class AlgorithmProblems {
+    public static void main (String[]args) {
+        System.out.println("Welcome to Algorithm Programs");
+    }
+}
