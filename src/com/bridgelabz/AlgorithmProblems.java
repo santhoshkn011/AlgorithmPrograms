7a186b86914f17082d13afda1ab703ac0761d553
Santhosh_Kumar_Nayak <santhoshkn011@gmail.com>
2022-07-04 10:05:06 +0530

    Added: Binary search program added.

--- src/com/bridgelabz/AlgorithmProblems.java
+++ src/com/bridgelabz/AlgorithmProblems.java
@@ -1,41 +1,35 @@
 /*
-Write static functions to return all permutations of a String using iterative method and
-Recursion method. Check if the arrays returned by two string functions are equal.
+Binary Search the Word from Word List
+a. Desc -> Read in a list of words from File. Then prompt the user to enter a word to
+search the list. The program reports if the search word is found in the list.
+b. I/P -> read in the list words comma separated from a File and then enter the word
+to be searched
+c. Logic -> Use Arrays to sort the word list and then do the binary search
+d. O/P -> Print the result if the word is found or not
  */
 package com.bridgelabz;
 
-import java.util.Scanner;
-
 public class AlgorithmProblems {
-    public static void main (String[]args) {
-        System.out.println("Enter your string: ");
-        Scanner sc = new Scanner(System.in);
-        String str = sc.next();
-        int n = str.length(); //length of the string
-        AlgorithmProblems permutation = new AlgorithmProblems(); //Created object
-        permutation.permute(str);
+    static String[] a = { "AB", "BC", "CD", "DE", "EF", "FG", "." };
+    static int min = 0;
+    static int max = a.length - 1;
+    static int mid;
+    static String key = "EF";
+    //main method
+    public static void main (String[]args){
+        System.out.println("Key Found at : " + stringBinarySearch() + " position");
     }
-    private void permute (String str) {
-        if (str == null || str.length() == 0) {
-            return;
+    public static int stringBinarySearch () {
+        while (min <= max) {
+            mid = (min + max) / 2;
+            if (a[mid].compareTo(key) < 0) {
+                min = mid + 1;
+            } else if (a[mid].compareTo(key) > 0) {
+                max = mid - 1;
+            } else {
+                return mid;
+            }
         }
-        permutations(str.toCharArray(), 0);
-    }
-    //method to permute the string
-    private static void permutations(char[] chars, int currentIndex) {
-        if (currentIndex == chars.length - 1) {
-            System.out.println(String.valueOf(chars));
-        }
-        for (int i = currentIndex; i < chars.length; i++) {
-            swap(chars, currentIndex, i);
-            permutations(chars, currentIndex + 1);
-            swap(chars, currentIndex, i);
-        }
-    }
-    //method to swap
-    private static void swap(char[] chars, int i, int j) {
-        char temp = chars[i];
-        chars[i] = chars[j];
-        chars[j] = temp;
+        return -1;
     }
 }
