5c8b68fc7d3ba7c75b462cbce1b87f726f0faead
Santhosh_Kumar_Nayak <santhoshkn011@gmail.com>
2022-07-04 10:16:56 +0530

    Added: Bubble Sort problem added.

--- src/com/bridgelabz/AlgorithmProblems.java
+++ src/com/bridgelabz/AlgorithmProblems.java
@@ -1,42 +1,31 @@
 /*
-Insertion Sort
-a. Desc -> Reads in strings and prints them in sorted order using insertion sort.
-b. I/P -> read in the list words
-c. Logic -> Use Insertion Sort to sort the words in the String array
-d. O/P -> Print the Sorted List
+Bubble Sort
+a. Desc -> Reads in integers prints them in sorted order using Bubble Sort
+b. I/P -> read in the list ints
+c. O/P -> Print the Sorted List
  */
 package com.bridgelabz;
 
 public class AlgorithmProblems {
-    /*Method to sort array using insertion sort*/
-    void sort(int arr[]) {
-        int n = arr.length;
-        for (int i = 1; i < n; ++i) {
-            int key = arr[i];
-            int j = i - 1;
-            /* Move elements of arr[0..i-1], that are
-               greater than key, to one position ahead
-               of their current position */
-            while (j >= 0 && arr[j] > key) {
-                arr[j + 1] = arr[j];
-                j = j - 1;
+    //Bubble sort method
+    public static void bubbleSort(int [] sort_arr, int len){
+        for (int i=0;i<len-1;++i){
+            for(int j=0;j<len-i-1; ++j){
+                if(sort_arr[j+1]<sort_arr[j]){
+                    int swap = sort_arr[j];
+                    sort_arr[j] = sort_arr[j+1];
+                    sort_arr[j+1] = swap;
+                }
             }
-            arr[j + 1] = key;
         }
     }
-    /* A method to print array of size n*/
-    static void printArray(int arr[]) {
-        int n = arr.length;
-        for
-            (int i = 0; i < n; ++i)
-            System.out.print(arr[i] + " ");
-        System.out.println();
-    }
-    // Main Method
-    public static void main(String args[]) {
-        int arr[] = { 4, 8, 77, 106, 1, 6, 99 };
-        AlgorithmProblems ob = new AlgorithmProblems(); //creating object
-        ob.sort(arr); //sorting the array
-        printArray(arr);
+    public static void main( String args[] ) {
+        int [] array = {5, 11, 6, 3, 22, 99, 110};
+        int len = array.length; //length of the array
+        bubbleSort(array,len); //calling the method
+
+        for(int i = 0; i<len; ++i){
+            System.out.print(array[i] + " ");
+        }
     }
 }
